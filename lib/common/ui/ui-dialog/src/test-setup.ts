d74abfe8412192111efbb03e473de7be45db295d
przemek <przemyslaw.leczycki@valueadd.pl>
2024-11-16 12:27:54 +0100

    feat: husky

--- lib/common/ui/ui-dialog/src/test-setup.ts
+++ lib/common/ui/ui-dialog/src/test-setup.ts
@@ -1,5 +1,6 @@
 import { TextEncoder, TextDecoder } from 'util';
 Object.assign(global, { TextDecoder, TextEncoder });
+import '@angular/localize/init';
 // @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
 globalThis.ngJest = {
   testEnvironmentOptions: {
